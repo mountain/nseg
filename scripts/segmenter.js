759698c5ecbc42c24e24256fc9f94fec5ca63cd5
Mingli Yuan <mingli.yuan@gmail.com>
2012-01-12 00:54:51 +0800

    a bunch of changes

--- scripts/segmenter.js
+++ scripts/segmenter.js
@@ -35,17 +35,17 @@ var touched = false, counterDir = 0;
 function handleDirAsync(dir, callback) {
     counterDir++;
     async
-        .readdir(dir)
+        .readdir(dir.path)
         .stat()
         .each(function (file) {
             if (file.stat.isDirectory()) {
                 var basepath = dir.basepath;
                 file.basepath = basepath;
-                file.target = path.relative(basepath, dir);
-                handleDirAsync(file.path, callback);
+                file.target = path.relative(basepath, dir.path);
+                handleDirAsync(file, callback);
             } else {
                 //console.log('path: ', file.path);
-                file.target = path.basename(file.path);
+                file.target = path.relative(dir.basepath, file.path);
                 callback(null, file);
             }
         })
@@ -68,7 +68,7 @@ function selectInputsAsync(inputs, callback) {
         .each(function (file) {
             if (file.stat.isDirectory()) {
                 file.basepath = file.path;
-                handleDirAsync(file.path, callback);
+                handleDirAsync(file, callback);
             } else {
                 //console.log('path: ', file.path);
                 file.target = path.basename(file.path);
@@ -78,45 +78,21 @@ function selectInputsAsync(inputs, callback) {
         .end();
 }
 
-function handleDirSync(dir, basepath, callback) {
-    counterDir++;
-    var inputs = fs.readdirSync(dir);
-    for (var i = 0, len = inputs.length; i < len; i++) {
-        var input = inputs[i];
-        var target = path.relative(basepath, dir);
-        if (path.existsSync(input)) {
-            var stat = fs.statSync(input);
-            if (stat.isDirectory()) {
-                handleDirSync(input, basepath, callback);
-            } else {
-                var file = {
-                    path: input,
-                    target: target
-                };
-                callback(null, file);
-            }
-        }
-    }
-    counterDir--;
-}
-
 function selectInputsSync(inputs, callback) {
     if (typeof inputs === 'string') {
         inputs = [inputs];
     }
-    for (var i = 0, len = inputs.length; i < len; i++) {
-        var input = inputs[i];
-        if (path.existsSync(input)) {
-            var stat = fs.statSync(input);
-            if (stat.isDirectory()) {
-                handleDirSync(input, input, callback);
-            } else {
-                var file = {
-                    path: input,
-                    target: path.basename(input)
-                };
-                callback(null, file);
-            }
+    var input = inputs[0];
+    if (path.existsSync(input)) {
+        var stat = fs.statSync(input);
+        if (stat.isDirectory()) {
+            throw 'target[' + input + '] is a directory! It should be a file.';
+        } else {
+            var file = {
+                path: input,
+                target: path.basename(input)
+            };
+            callback(null, file);
         }
     }
 }
@@ -184,9 +160,99 @@ function execute(segmenter, limits) {
     throttled();
 }
 
+/**
+ * Offers functionality similar to mkdir -p
+ * Orinally at https://gist.github.com/742162
+ * Modified for our own purpose
+ *
+ * Asynchronous operation. No arguments other than a possible exception
+ * are given to the completion callback.
+ */
+function mkdir_p(filepath, mode, callback, position) {
+    mode = mode || 0777;
+    position = position || 0;
+    var parts = require('path').normalize(filepath).split('/');
+    parts = parts.slice(0, parts.length - 1);
+
+    if (position >= parts.length) {
+        if (callback) {
+            return callback();
+        } else {
+            return true;
+        }
+    }
+
+    var directory = parts.slice(0, position + 1).join('/');
+    if (directory) {
+        fs.stat(directory, function (err) {
+            if (err === null) {
+                mkdir_p(filepath, mode, callback, position + 1);
+            } else {
+                fs.mkdir(directory, mode, function (error) {
+                    //if (error) {
+                    //    if (callback) {
+                    //        return callback(error);
+                    //    } else {
+                    //        throw error;
+                    //    }
+                    //} else {
+                        mkdir_p(filepath, mode, callback, position + 1);
+                    //}
+                });
+            }
+        });
+    }
+}
+
+function safeEnqueue(target, input) {
+    mkdir_p(target, null, function (err) {
+            if (err) {
+                console.log('err:', target);
+                throw err;
+            }
+            enqueue(target, input);
+        }
+    );
+}
+
 exports.VERSION = mmseg.VERSION;
 
-exports.seg = function (options, text) {
+exports.segf = function (options) {
+
+    var dictionary, frequency;
+    if (options.dictionary) {
+        dictionary = require(loadpath(__dirname, options.dictionary));
+    }
+    if (options.frequency) {
+        frequency = require(loadpath(__dirname, options.frequency));
+    }
+
+    async
+        .files([options.output])
+        .exists()
+        .filter(function (file) {
+            return !file.exists;
+        })
+        .stat()
+        .each(function (output) {
+            selectInputsSync(options.inputs, function (err, input) {
+                if (err) {
+                    throw err;
+                }
+                safeEnqueue(output.path, input);
+                touched = true;
+            });
+        })
+        .end();
+
+    var segmenter = mmseg.evented({
+        dict: dictionary || dict,
+        freq: frequency || freq
+    });
+    execute(segmenter, options.limits || 1);
+};
+
+exports.segd = function (options) {
 
     var dictionary, frequency;
     if (options.dictionary) {
@@ -209,30 +275,24 @@ exports.seg = function (options, text) {
                     if (err) {
                         throw err;
                     }
-                    var target = path.resolve(output, input.target);
+                    var target = path.resolve(output.path, input.target);
+                    target = path.relative(process.cwd(), target);
+                    //console.log('input.target: ', input.target);
                     //console.log('target: ', target);
+                    //console.log('cwd: ', process.cwd());
+
                     path.exists(target, function (exists) {
                         if (exists) {
                             throw 'file[' + target + '] exists! conflict should be resolved.';
                         } else {
                             //console.log('enqueue: ', target, input);
-                            enqueue(target, input);
+                            safeEnqueue(target, input);
                         }
                         touched = true;
                     });
                 });
             } else {
-                selectInputsSync(options.inputs, function (err, input) {
-                    if (err) {
-                        throw err;
-                    }
-                    var target = path.resolve(output, input.target);
-                    if (path.existsSync(target)) {
-                        throw 'file[' + target + '] exists! conflict should be resolved.';
-                    }
-                    enqueue(target, input);
-                    touched = true;
-                });
+                throw 'target[' + output.path + '] is a file! please use segf command.';
             }
         })
         .end();
